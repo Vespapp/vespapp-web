e36462432d549894e434f195dcbcc80267ef09b4
CarlosTenorio <carlostenorioperez@hotmail.com>
2017-05-22 13:14:06 +0200

    Add info modal when select location on new sighting

---
+++ src/js/new_sighting.js
@@ -0,0 +1,8 @@
+$( document ).ready(function() {
+
+  // When select location display info modal
+  $('#selectLotacion').on('change', function (e) {
+    $('#modalInfoLocation').modal('toggle');
+  });
+
+});
